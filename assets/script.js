719159e9d55d4d518bb4ecefaee824ff85bba234
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2023-02-22 17:49:49 -0800

    forgot to commit, added javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,66 @@
 // Variables
-var numbers = '0123456789';
-var lowerAlpha
+var numbers = '123456789';
+var lowerAlpha = 'abcdefghijklmnopqrstuvwxyz';
+var upperAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var symbols = '~!@#$%^&*()_+=-`[]{}\\|;,./?<>';
 
+var slider = document.getElementById('passwordLengthRange');
+var output = document.getElementById('number');
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+var generatePassword = function() {
+    var length = slider.value;
+    var confirmLower = document.getElementById('lowercase').checked;
+    var confirmUpper = document.getElementById('uppercase').checked;
+    var confirmNumber = document.getElementById('numbers').checked;
+    var confirmSymbol = document.getElementById('special').checked;
+
+    var charBuild = '';
+    var generatedPassword = '';
+
+    var countOptions = confirmLower + confirmUpper + confirmNumber + confirmSymbol;
+
+    if (countOptions === 0) {
+        generatedPassword = generatedPassword + 'Please select at least one option!';
+        return generatedPassword;
+    }
+
+    if (confirmLower) {
+        charBuild = charBuild.concat(lowerAlpha);
+    }
+
+    if (confirmUpper) {
+        charBuild = charBuild.concat(upperAlpha);
+    }
+
+    if (confirmNumber) {
+        charBuild = charBuild.concat(numbers);
+    }
+
+    if (confirmSymbol) {
+        charBuild = charBuild.concat(symbols);
+    }
+
+
+    for (i=0; i < length; i++) {
+        var randomi = Math.floor(Math.random() * charBuild.length);
+        generatedPassword += charBuild[randomi];
+    }
+
+    return generatedPassword;
+};
+
+
+
+// function syncPasswordLength(e) {
+//     var value = e.target.value
+//     passwordLengthNumber.value = value
+//     passwordLengthRange.value = value
+// }
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
